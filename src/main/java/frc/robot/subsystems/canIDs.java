9aea6eb35bde1a5678b00416e3e65275b8073306
Roger <161100397+AstrayComett@users.noreply.github.com>
2026-07-18 08:07:00 -0500

    Fix Attempts

--- src/main/java/frc/robot/subsystems/canIDs.java
+++ src/main/java/frc/robot/subsystems/canIDs.java
@@ -7,6 +7,6 @@ public class canIDs {
     public static int shooterFlywheelACANID = 17;
     public static int shooterFlywheelBCANID = 18;
     public static int shooterHoodCANID = 19; //make sure in code to make neo and not kracken
-    public static int intakeExtendCANID = 20;
+    public static int intakeExtendCANID_A = 20;
     public static int intakeCANID = 22;
 }
